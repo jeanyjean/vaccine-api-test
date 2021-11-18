4ea047670adabd5465d8fb78f56ddca76f714466
jeanyjean <purich.double@gmail.com>
2021-11-18 12:32:19 +0700

    Add more tests

--- test_reservation_api.py
+++ test_reservation_api.py
@@ -20,6 +20,46 @@ class ReservationApiTest(unittest.TestCase):
         """
         self.assertEqual(self.response.status_code, 200)
     
+    def test_reservation_id_get_reservation_from_dates_successfully(self):
+        """
+        Test the reservation_id of getting the reservation from the date successfully.
+        """
+        self.response_content = self.response.json()
+
+        self.assertEqual(self.response_content[0]["reservation_id"], 8)
+    
+    def test_register_timestamp_get_reservation_from_dates_successfully(self):
+        """
+        Test the register_timestamp of getting the reservation from the date successfully.
+        """
+        self.response_content = self.response.json()
+
+        self.assertEqual(self.response_content[0]["register_timestamp"], "2021-10-20T17:12:39.738000")
+    
+    def test_vaccinated_get_reservation_from_dates_successfully(self):
+        """
+        Test the vaccinated of getting the reservation from the date successfully.
+        """
+        self.response_content = self.response.json()
+
+        self.assertEqual(self.response_content[0]["vaccinated"], False)
+    
+    def test_owner_get_reservation_from_dates_successfully(self):
+        """
+        Test the owner of getting the reservation from the date successfully.
+        """
+        self.response_content = self.response.json()
+        self.owner = {
+                        "name": "foo",
+                        "surname": "rock",
+                        "birth_date": "2001-11-18",
+                        "citizen_id": "1234567848204",
+                        "occupation": "programmer",
+                        "address": "bangkok"
+                    }
+
+        self.assertEqual(self.response_content[0]["owner"], self.owner)
+
     def test_get_reservation_from_dates_wrong_parameters_type(self):
         """
         Test the status code of getting the reservation from the date with the wrong parameters type.
@@ -44,9 +84,15 @@ class ReservationApiTest(unittest.TestCase):
 
     def test_get_reservation_from_dates_content_type(self):
         """
-        Test the header of getting the reservation from the date successfully.
+        Test the header content-type of getting the reservation from the date successfully.
         """
         self.assertEqual(self.response.headers["Content-Type"], "application/json")
+    
+    def test_get_reservation_from_dates_header_server(self):
+        """
+        Test the header server of getting the reservation from the date successfully.
+        """
+        self.assertEqual(self.response.headers["Server"], "uvicorn")
     
 if __name__ == '__main__':
     unittest.main()
